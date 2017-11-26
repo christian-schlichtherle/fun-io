eb338e4092977742566423753d2dcfa15323c806
Christian Schlichtherle <christian@schlichtherle.de>
2017-11-26 11:19:26 +0400

    Emits CSS in their own file.

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,26 @@
+const ExtractTextWebpackPlugin = require("extract-text-webpack-plugin");
 const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-    entry: path.resolve(__dirname, 'webpack', 'index.js'),
+    entry: path.resolve(__dirname, 'webpack', 'entry.js'),
     module: {
         rules: [
             {
                 test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
+                use: ExtractTextWebpackPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
             }
         ]
     },
     output: {
-        path: path.resolve(__dirname, 'assets', 'javascripts'),
-        filename: 'bundle.js'
+        path: path.resolve(__dirname, 'assets', 'bundle'),
+        filename: 'main.js'
     },
     plugins: [
+        new ExtractTextWebpackPlugin("main.css"),
         new webpack.ProvidePlugin({
             $: 'jquery',
             jQuery: 'jquery',
