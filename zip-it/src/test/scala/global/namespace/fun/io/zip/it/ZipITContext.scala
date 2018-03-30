be71a4c7b460512ec8fd87173f13360f474b59b4
Christian Schlichtherle <christian@schlichtherle.de>
2018-03-30 23:04:43 +0400

    Test cleanup.

--- zip-it/src/test/scala/global/namespace/fun/io/zip/it/ZipITContext.scala
+++ zip-it/src/test/scala/global/namespace/fun/io/zip/it/ZipITContext.scala
@@ -5,46 +5,66 @@
 package global.namespace.fun.io.zip.it
 
 import java.io.File
+import java.nio.charset.Charset
 import java.security.MessageDigest
+import java.util.logging.{Level, Logger}
 
+import global.namespace.fun.io.api.{Codec, Store}
+import global.namespace.fun.io.bios.BIOS.memoryStore
+import global.namespace.fun.io.jaxb.JAXB
 import global.namespace.fun.io.scala.api._
 import global.namespace.fun.io.zip.diff.ZipDiffEngine
 import global.namespace.fun.io.zip.io.{JarStore, MessageDigests, ZipInput}
-import global.namespace.fun.io.zip.model.DeltaModel
-import javax.xml.bind.JAXBContext
+import global.namespace.fun.io.zip.it.ZipITContext._
+import global.namespace.fun.io.zip.model.DeltaModel.jaxbContext
+import javax.xml.bind.{Marshaller, Unmarshaller}
+import org.scalatest.Matchers.{equal, theSameInstanceAs, _}
 
 /** @author Christian Schlichtherle */
-trait ZipITContext extends ITContext {
+trait ZipITContext {
 
-  def loanZipDiffEngine[A](fun: ZipDiffEngine => A): A =
-    loanTestJars { (archive1, archive2) =>
-      fun(new ZipDiffEngine {
-
-        lazy val digest: MessageDigest = MessageDigests.sha1
-
-        def input1: ZipInput = archive1
-
-        def input2: ZipInput = archive2
-      })
-    }
-
-  def loanTestJars[A](fun: (ZipInput, ZipInput) => A): A = {
+  final def loanZipDiffEngine[A](fun: ZipDiffEngine => A): A = {
     testJarStore1.applyReader[A] { jar1: ZipInput =>
       testJarStore2.applyReader[A] { jar2: ZipInput =>
-        fun(jar1, jar2)
+        fun(new ZipDiffEngine {
+
+          lazy val digest: MessageDigest = MessageDigests.sha1
+
+          def input1: ZipInput = jar1
+
+          def input2: ZipInput = jar2
+        })
       }
     }
   }
 
-  final lazy val testJarStore1: JarStore = new JarStore(testJarFile1)
+  final lazy val testJarStore1: JarStore = new JarStore(file("test1.jar"))
 
-  final def testJarFile1: File = file("test1.jar")
+  final lazy val testJarStore2: JarStore = new JarStore(file("test2.jar"))
 
-  final lazy val testJarStore2: JarStore = new JarStore(testJarFile2)
+  final def assertRoundTripXmlSerializable(original: AnyRef) {
+    val store = memoryStore
+    val clone = jaxbCodec connect store clone original
+    logger.log(Level.FINE, "\n{0}", utf8String(store))
+    clone should equal (original)
+    clone should not be theSameInstanceAs (original)
+  }
+}
 
-  final def testJarFile2: File = file("test2.jar")
+/** @author Christian Schlichtherle */
+private object ZipITContext {
 
-  private def file(resourceName: String) = new File((classOf[ZipITContext] getResource resourceName).toURI)
+  def file(resourceName: String) = new File((classOf[ZipITContext] getResource resourceName).toURI)
 
-  lazy val jaxbContext: JAXBContext = DeltaModel.jaxbContext
+  val jaxbCodec: Codec = JAXB.xmlCodec(jaxbContext, modifyMarshaller _, unmarshallerModifier _)
+
+  private def modifyMarshaller(m: Marshaller): Unit = m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true)
+
+  private def unmarshallerModifier(u: Unmarshaller): Unit = { }
+
+  val logger: Logger = Logger.getLogger(getClass.getName)
+
+  def utf8String(store: Store): String = new String(store.content, utf8)
+
+  private val utf8: Charset = Charset forName "UTF-8"
 }
