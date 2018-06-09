65904f9f55cefb55a1766cf1bed9d3cfc8fc7037
Christian Schlichtherle <christian@schlichtherle.de>
2018-06-09 20:19:39 +0400

    Upgrades dependencies.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,17 +20,17 @@ object Dependencies {
   private val JAXB_Version = "2.3.0"
 
   val ActivationApi: ModuleID = "javax.activation" % "javax.activation-api" % "1.2.0"
-  val CommonsCompress: ModuleID = "org.apache.commons" % "commons-compress" % "1.16.1"
+  val CommonsCompress: ModuleID = "org.apache.commons" % "commons-compress" % "1.17"
   val JacksonDatabind: ModuleID = "com.fasterxml.jackson.core" % "jackson-databind" % "2.9.5"
   val JaxbApi: ModuleID = "javax.xml.bind" % "jaxb-api" % JAXB_Version
   val JaxbRuntime: ModuleID = "org.glassfish.jaxb" % "jaxb-runtime" % JAXB_Version
-  val MockitoCore: ModuleID = "org.mockito" % "mockito-core" % "2.18.0"
-  val Scalacheck: ModuleID = "org.scalacheck" %% "scalacheck" % "1.13.5"
+  val MockitoCore: ModuleID = "org.mockito" % "mockito-core" % "2.18.3"
+  val Scalacheck: ModuleID = "org.scalacheck" %% "scalacheck" % "1.14.0"
   val Scalatest: ModuleID = "org.scalatest" %% "scalatest" % "3.0.5"
   val Xz: ModuleID = "org.tukaani" % "xz" % "1.8"
-  val ZstdJni: ModuleID = "com.github.luben" % "zstd-jni" % "1.3.4-4"
+  val ZstdJni: ModuleID = "com.github.luben" % "zstd-jni" % "1.3.4-10"
 
   val ScalaVersion_2_10: String = sys.env.getOrElse("SCALA_VERSION_2_10", "2.10.7")
   val ScalaVersion_2_11: String = sys.env.getOrElse("SCALA_VERSION_2_11", "2.11.12")
-  val ScalaVersion_2_12: String = sys.env.getOrElse("SCALA_VERSION_2_12", "2.12.4")
+  val ScalaVersion_2_12: String = sys.env.getOrElse("SCALA_VERSION_2_12", "2.12.6")
 }
