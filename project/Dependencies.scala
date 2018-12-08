cec94d8328f563cb9f6c9bb1bdcc4fedd8c1a5e2
Christian Schlichtherle <christian@schlichtherle.de>
2018-12-08 20:04:55 +0400

    Updates build definition.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -23,7 +23,7 @@ object Dependencies {
   val JacksonDatabind: ModuleID = "com.fasterxml.jackson.core" % "jackson-databind" % "2.9.7"
   val JaxbApi: ModuleID = "javax.xml.bind" % "jaxb-api" % JAXB_Version
   val JaxbRuntime: ModuleID = "org.glassfish.jaxb" % "jaxb-runtime" % JAXB_Version
-  val MockitoCore: ModuleID = "org.mockito" % "mockito-core" % "2.23.0"
+  val MockitoCore: ModuleID = "org.mockito" % "mockito-core" % "2.23.4"
   val S3: ModuleID = "software.amazon.awssdk" % "s3" % "2.0.0-preview-12"
   val Scalacheck: ModuleID = "org.scalacheck" %% "scalacheck" % "1.14.0"
   val Scalatest: ModuleID = "org.scalatest" %% "scalatest" % "3.0.5"
