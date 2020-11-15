325df507baf00cdcef386a6773caaaff79bb1122
Christian Schlichtherle <christian@schlichtherle.de>
2020-11-15 15:31:43 +0100

    Updates dependencies.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,21 +17,21 @@ import sbt._
 
 object Dependencies {
 
-  private val JAXB_Version = "2.3.1"
+  private val JAXB_Version = "2.3.3"
 
-  val AwsJavaSdkS3: ModuleID = "com.amazonaws" % "aws-java-sdk-s3" % "1.11.841"
+  val AwsJavaSdkS3: ModuleID = "com.amazonaws" % "aws-java-sdk-s3" % "1.11.901"
   val CommonsCompress: ModuleID = "org.apache.commons" % "commons-compress" % "1.20"
-  val Config: ModuleID = "com.typesafe" % "config" % "1.4.0"
-  val JacksonDatabind: ModuleID = "com.fasterxml.jackson.core" % "jackson-databind" % "2.11.2"
-  val JaxbApi: ModuleID = "javax.xml.bind" % "jaxb-api" % JAXB_Version
+  val Config: ModuleID = "com.typesafe" % "config" % "1.4.1"
+  val JacksonDatabind: ModuleID = "com.fasterxml.jackson.core" % "jackson-databind" % "2.11.3"
+  val JaxbApi: ModuleID = "jakarta.xml.bind" % "jakarta.xml.bind-api" % JAXB_Version
   val JaxbRuntime: ModuleID = "org.glassfish.jaxb" % "jaxb-runtime" % JAXB_Version
-  val MockitoCore: ModuleID = "org.mockito" % "mockito-core" % "3.4.6"
-  val S3: ModuleID = "software.amazon.awssdk" % "s3" % "2.13.75"
-  val Scalatest: ModuleID = "org.scalatest" %% "scalatest" % "3.2.1"
-  val ScalatestplusMockito = "org.scalatestplus" %% "mockito-3-4" % "3.2.1.0"
+  val MockitoCore: ModuleID = "org.mockito" % "mockito-core" % "3.6.0"
+  val S3: ModuleID = "software.amazon.awssdk" % "s3" % "2.15.28"
+  val Scalatest: ModuleID = "org.scalatest" %% "scalatest" % "3.2.3"
+  val ScalatestplusMockito = "org.scalatestplus" %% "mockito-3-4" % "3.2.3.0"
   val Slf4jSimple: ModuleID = "org.slf4j" % "slf4j-simple" % "1.7.30"
   val Xz: ModuleID = "org.tukaani" % "xz" % "1.8"
-  val ZstdJni: ModuleID = "com.github.luben" % "zstd-jni" % "1.4.5-6"
+  val ZstdJni: ModuleID = "com.github.luben" % "zstd-jni" % "1.4.5-12"
 
   val ScalaVersion_2_10: String = sys.env.getOrElse("SCALA_VERSION_2_10", "2.10.7")
   val ScalaVersion_2_11: String = sys.env.getOrElse("SCALA_VERSION_2_11", "2.11.12")
